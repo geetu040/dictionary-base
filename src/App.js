b46c8e9f716426a99c283529f3c88c8903d57eba
geetu040 <raoarmaghanshakir040@gmail.com>
2022-12-12 21:00:43 +0500

    fixed build error: vocab0 variable assigned but never used

--- src/App.js
+++ src/App.js
@@ -69,74 +69,74 @@ export default App;
 
 
 
-let vocab0 = [{
-	"word": "word1",
-	"meaning": "meaning1",
-	"time": 1667878568.6029875,
-	"labels": ["l1", "l4"],
-	"keys": ["key911", "key426", "key300", "key316", "key132", "key521"],
-	"vals": ["val911", "val426", "val300", "val316", "val132", "val521"]
-}, {
-	"word": "word2",
-	"meaning": "meaning2",
-	"time": 1667878568.6029875,
-	"labels": ["l1", "l3"],
-	"keys": ["key656", "key362", "key280"],
-	"vals": ["val656", "val362", "val280"]
-}, {
-	"word": "word3",
-	"meaning": "meaning3",
-	"time": 1667878568.6029875,
-	"labels": ["l1", "l2", "l4"],
-	"keys": [],
-	"vals": []
-}, {
-	"word": "word8",
-	"meaning": "meaning8",
-	"time": 1667878568.6029875,
-	"labels": ["l4", "l3"],
-	"keys": ["key912", "key850"],
-	"vals": ["val912", "val850"]
-}, {
-	"word": "word7",
-	"meaning": "meaning7",
-	"time": 1667878568.6029875,
-	"labels": ["l2"],
-	"keys": [],
-	"vals": []
-}, {
-	"word": "word4",
-	"meaning": "meaning4",
-	"time": 1667878568.6029875,
-	"labels": ["l4", "l2"],
-	"keys": ["key792", "key382"],
-	"vals": ["val792", "val382"]
-}, {
-	"word": "word5",
-	"meaning": "meaning5",
-	"time": 1667878568.6029875,
-	"labels": [],
-	"keys": [],
-	"vals": []
-}, {
-	"word": "word6",
-	"meaning": "meaning6",
-	"time": 1667878568.6029875,
-	"labels": ["l3", "l4", "l2"],
-	"keys": ["key466", "key280", "key429", "key751"],
-	"vals": ["val466", "val280", "val429", "val751"]
-}, {
-	"word": "word9",
-	"meaning": "meaning9",
-	"time": 1667878568.6029875,
-	"labels": ["l2"],
-	"keys": ["key24", "key698", "key681", "key592", "key81", "key667"],
-	"vals": ["val24", "val698", "val681", "val592", "val81", "val667"]
-}, {
-	"word": "word0",
-	"meaning": "meaning0",
-	"time": 1667878568.6029875,
-	"labels": ["l2", "l1", "l4"],
-	"keys": ["key717", "key689", "key478", "key724"],
-	"vals": ["val717", "val689", "val478", "val724"]
-}]
+// let vocab0 = [{
+// 	"word": "word1",
+// 	"meaning": "meaning1",
+// 	"time": 1667878568.6029875,
+// 	"labels": ["l1", "l4"],
+// 	"keys": ["key911", "key426", "key300", "key316", "key132", "key521"],
+// 	"vals": ["val911", "val426", "val300", "val316", "val132", "val521"]
+// }, {
+// 	"word": "word2",
+// 	"meaning": "meaning2",
+// 	"time": 1667878568.6029875,
+// 	"labels": ["l1", "l3"],
+// 	"keys": ["key656", "key362", "key280"],
+// 	"vals": ["val656", "val362", "val280"]
+// }, {
+// 	"word": "word3",
+// 	"meaning": "meaning3",
+// 	"time": 1667878568.6029875,
+// 	"labels": ["l1", "l2", "l4"],
+// 	"keys": [],
+// 	"vals": []
+// }, {
+// 	"word": "word8",
+// 	"meaning": "meaning8",
+// 	"time": 1667878568.6029875,
+// 	"labels": ["l4", "l3"],
+// 	"keys": ["key912", "key850"],
+// 	"vals": ["val912", "val850"]
+// }, {
+// 	"word": "word7",
+// 	"meaning": "meaning7",
+// 	"time": 1667878568.6029875,
+// 	"labels": ["l2"],
+// 	"keys": [],
+// 	"vals": []
+// }, {
+// 	"word": "word4",
+// 	"meaning": "meaning4",
+// 	"time": 1667878568.6029875,
+// 	"labels": ["l4", "l2"],
+// 	"keys": ["key792", "key382"],
+// 	"vals": ["val792", "val382"]
+// }, {
+// 	"word": "word5",
+// 	"meaning": "meaning5",
+// 	"time": 1667878568.6029875,
+// 	"labels": [],
+// 	"keys": [],
+// 	"vals": []
+// }, {
+// 	"word": "word6",
+// 	"meaning": "meaning6",
+// 	"time": 1667878568.6029875,
+// 	"labels": ["l3", "l4", "l2"],
+// 	"keys": ["key466", "key280", "key429", "key751"],
+// 	"vals": ["val466", "val280", "val429", "val751"]
+// }, {
+// 	"word": "word9",
+// 	"meaning": "meaning9",
+// 	"time": 1667878568.6029875,
+// 	"labels": ["l2"],
+// 	"keys": ["key24", "key698", "key681", "key592", "key81", "key667"],
+// 	"vals": ["val24", "val698", "val681", "val592", "val81", "val667"]
+// }, {
+// 	"word": "word0",
+// 	"meaning": "meaning0",
+// 	"time": 1667878568.6029875,
+// 	"labels": ["l2", "l1", "l4"],
+// 	"keys": ["key717", "key689", "key478", "key724"],
+// 	"vals": ["val717", "val689", "val478", "val724"]
+// }]
